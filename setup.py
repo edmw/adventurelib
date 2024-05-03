9597da1d98eba6a0156a589c802610b39f5c7cd2
Michael <mib@michael-baumgaertner.de>
2024-05-03 21:20:22 +0200

    Add rich version to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from os.path import abspath, dirname, join
-from setuptools import setup
-from adventurelib import __version__
 
+from setuptools import setup
+
+from adventurelib import __version__
 
 ROOT = abspath(dirname(__file__))
 
@@ -10,30 +11,28 @@ with open(join(ROOT, "README.md")) as fd:
     README = fd.read()
 
 setup(
-    name='adventurelib',
-    description='Easy text adventures',
+    name="adventurelib",
+    description="Easy text adventures",
     long_description=README,
     long_description_content_type="text/markdown",
     version=__version__,
-    author='Daniel Pope',
-    author_email='mauve@mauveweb.co.uk',
-    url='https://github.com/lordmauve/adventurelib',
-    project_urls={
-        'Documentation': 'https://adventurelib.readthedocs.io/'
-    },
-    py_modules=['adventurelib'],
+    author="Daniel Pope",
+    author_email="mauve@mauveweb.co.uk",
+    url="https://github.com/lordmauve/adventurelib",
+    project_urls={"Documentation": "https://adventurelib.readthedocs.io/"},
+    py_modules=["adventurelib", "adventurelib_rich"],
     extras_require={
         ':python_version < "3.3"': [
-            'backports.shutil_get_terminal_size>=1.0.0',
+            "backports.shutil_get_terminal_size>=1.0.0",
         ],
     },
-    python_requires='>=3',
+    python_requires=">=3",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Education',
-        'Topic :: Games/Entertainment',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment",
+    ],
 )
